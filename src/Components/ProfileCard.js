bf8b5fde0a6842b742fd4c667fcc8d709857b336
ruthw <rwustenberg@gmail.com>
2019-10-08 06:25:15 -0500

    profile done

--- src/Components/ProfileCard.js
+++ src/Components/ProfileCard.js
@@ -1,7 +1,7 @@
 import React from "react";
-import '../Styles/profile-card.css'
-import moment from "moment"
-import {Link} from 'react-router-dom'
+import "../Styles/profile-card.css";
+import moment from "moment";
+import { Link } from "react-router-dom";
 
 class ProfileCard extends React.Component {
   render() {
@@ -16,16 +16,19 @@ class ProfileCard extends React.Component {
         </p>
         <p>
           <i className="far fa-calendar-check" style={{ color: "#EF5A00" }}></i>{" "}
-          Date: {moment(this.props.startDetails).format('D MMMM')}
+          Date: {moment(this.props.startDetails).format("D MMMM")}
         </p>
         <p>
-          <i className="fas fa-clock" style={{ color: "#EF5A00" }}></i> Doors: {moment(this.props.startDetails).format('HH:mm')}
+          <i className="fas fa-clock" style={{ color: "#EF5A00" }}></i> Doors:{" "}
+          {moment(this.props.startDetails).format("HH:mm")}
         </p>
 
-				<Link to={`/qr/${this.props.randomNumber}`}>
-				Click here to access your QR code to enter the event
-				</Link>
-
+        <Link
+          to={`/qr/${this.props.randomNumber}`}
+          style={{ color: "#EF5A00", textAlign: "center" }}
+        >
+          Click here to access your QR code to enter the event
+        </Link>
       </div>
     );
   }
